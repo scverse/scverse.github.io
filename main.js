61877533b720b2150d2923494ad9cb142c4b91f0
ivirshup <ivirshup@users.noreply.github.com>
2022-11-16 18:25:13 +0000

    deploy: e8d5015903e9fee927a3e85d5c45c711db199c2a

---
+++ main.js
@@ -0,0 +1,13 @@
+const filterPackages = () => {
+  const trs = document.querySelectorAll('.eco-table-row');
+  const filter = document.querySelector('#eco-filter').value;
+  const regex = new RegExp(filter, 'i');
+  const tdFound = td => regex.test(td.innerHTML);
+  const pkgFound = childrenArr => childrenArr.some(tdFound);
+  const toggleTrs = ({ style, children }) => {
+    style.display = pkgFound([
+      ...children
+    ]) ? '' : 'none' ;
+  };
+  trs.forEach(toggleTrs);
+}
